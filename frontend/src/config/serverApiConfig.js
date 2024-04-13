f2890c1121f18e692c83411c1a497a1acf4cf8a1
IamHarsh05 <harshchaudhari592@gmail.com>
2024-04-13 13:30:27 +0530

    Commmit

--- frontend/src/config/serverApiConfig.js
+++ frontend/src/config/serverApiConfig.js
@@ -1,11 +1,11 @@
 export const API_BASE_URL =
   import.meta.env.PROD || import.meta.env.VITE_DEV_REMOTE == 'remote'
     ? import.meta.env.VITE_BACKEND_SERVER + 'api/'
-    : 'https://account-erp.onrender.com/api/';
+    : 'http://localhost:8888/api/';
 export const BASE_URL =
   import.meta.env.PROD || import.meta.env.VITE_DEV_REMOTE
     ? import.meta.env.VITE_BACKEND_SERVER
-    : 'https://account-erp.onrender.com';
+    : 'http://localhost:8888/';
 
 export const WEBSITE_URL = import.meta.env.PROD
   ? 'http://cloud.erpapp.com/'
